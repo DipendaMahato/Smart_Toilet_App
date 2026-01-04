e3115760799fa3a63c72fbc7c973e321a1c23efb
Dipendra Mahato <dipendram891@gmail.com>
2026-01-04 18:36:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4897018311-c7624",
+  "appId": "1:231722190452:web:e7e1ff5d19fbc91871b321",
+  "apiKey": "AIzaSyBHyHUEKnwm9PJdmkGLMSeUobDAMV3ubUI",
+  "authDomain": "studio-4897018311-c7624.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "231722190452"
+};
