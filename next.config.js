b2fea78a48b3ec906fd9466a78f4704824d4769f
Dipendra Mahato <144335845+DipendaMahato@users.noreply.github.com>
2026-01-26 09:57:53 +0530

    Add Next.js configuration for Smart Toilet App

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/Smart_Toilet_App",
+  images: { unoptimized: true },
+};
+
+module.exports = nextConfig;
